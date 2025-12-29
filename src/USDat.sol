8e1e16f3e7bf667f6df61da59b4f2c8dcd257a4a
Sebastian Melendez <sebm@seas.upenn.edu>
2025-12-28 22:29:16 -0700

    update to use storage location

--- src/USDat.sol
+++ src/USDat.sol
@@ -5,12 +5,8 @@ pragma solidity ^0.8.27;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import {
-    ERC20BurnableUpgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import {
-    ERC20PermitUpgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -27,10 +23,23 @@ contract USDat is
 {
     using SafeERC20 for IERC20;
 
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BLACKLIST_MANAGER_ROLE = keccak256("BLACKLIST_MANAGER_ROLE");
+    bytes32 public constant PROCESSOR_ROLE = keccak256("PROCESSOR_ROLE");
+    bytes32 public constant COMPLIANCE_ROLE = keccak256("COMPLIANCE_ROLE");
 
-    mapping(address => bool) private _blacklisted;
+    /// @custom:storage-location erc7201:saturn.usdat.storage
+    struct USDatStorage {
+        mapping(address => bool) blacklisted;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("saturn.usdat.storage")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant USDAT_STORAGE_LOCATION =
+        0xd319317dee4509e78244ea42ada3c23f49b19eec2b552d2c52af685634fde100;
+
+    function _getUSDatStorage() private pure returns (USDatStorage storage $) {
+        assembly {
+            $.slot := USDAT_STORAGE_LOCATION
+        }
+    }
 
     event Blacklisted(address indexed account);
     event UnBlacklisted(address indexed account);
@@ -40,7 +49,11 @@ contract USDat is
         _disableInitializers();
     }
 
-    function initialize(address defaultAdmin, address minter, address blacklistManager) public initializer {
+    function initialize(
+        address defaultAdmin,
+        address processor,
+        address compliance
+    ) public initializer {
         __ERC20_init("USDat", "USDat");
         __ERC20Burnable_init();
         __AccessControl_init();
@@ -49,67 +62,80 @@ contract USDat is
         // Manages Upgrades
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         // Manages minting new tokens
-        _grantRole(MINTER_ROLE, minter);
-        // Manages
-        _grantRole(BLACKLIST_MANAGER_ROLE, blacklistManager);
+        _grantRole(PROCESSOR_ROLE, processor);
+        // Manages compliance (blacklist)
+        _grantRole(COMPLIANCE_ROLE, compliance);
     }
 
     function _requireNotBlacklisted(address account) internal view {
-        require(!_blacklisted[account], "Recipient is blacklisted");
+        USDatStorage storage $ = _getUSDatStorage();
+        require(!$.blacklisted[account], "Recipient is blacklisted");
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) public onlyRole(PROCESSOR_ROLE) {
         _requireNotBlacklisted(to);
         _mint(to, amount);
     }
 
-    function rescueTokens(address token, uint256 amount, address to)
-        external
-        nonReentrant
-        onlyRole(BLACKLIST_MANAGER_ROLE)
-    {
+    function rescueTokens(
+        address token,
+        uint256 amount,
+        address to
+    ) external nonReentrant onlyRole(COMPLIANCE_ROLE) {
         IERC20(token).safeTransfer(to, amount);
     }
 
-    function transfer(address to, uint256 amount) public override returns (bool) {
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
         _requireNotBlacklisted(to);
         return super.transfer(to, amount);
     }
 
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
         _requireNotBlacklisted(to);
         return super.transferFrom(from, to, amount);
     }
 
-    function burnBlacklistedTokens(address account) public onlyRole(BLACKLIST_MANAGER_ROLE) {
-        require(_blacklisted[account], "Account is not blacklisted");
+    function burnBlacklistedTokens(
+        address account
+    ) public onlyRole(COMPLIANCE_ROLE) {
+        USDatStorage storage $ = _getUSDatStorage();
+        require($.blacklisted[account], "Account is not blacklisted");
         uint256 amount = balanceOf(account);
         require(amount > 0, "Account has no balance");
         _burn(account, amount);
     }
 
-    function addToBlacklist(address account) external onlyRole(BLACKLIST_MANAGER_ROLE) {
-        if (_blacklisted[account]) revert("Already blacklisted");
-        _blacklisted[account] = true;
+    function addToBlacklist(
+        address account
+    ) external onlyRole(COMPLIANCE_ROLE) {
+        USDatStorage storage $ = _getUSDatStorage();
+        if ($.blacklisted[account]) revert("Already blacklisted");
+        $.blacklisted[account] = true;
         emit Blacklisted(account);
     }
 
-    function removeFromBlacklist(address account) external onlyRole(BLACKLIST_MANAGER_ROLE) {
-        if (!_blacklisted[account]) revert("Not blacklisted");
-        _blacklisted[account] = false;
+    function removeFromBlacklist(
+        address account
+    ) external onlyRole(COMPLIANCE_ROLE) {
+        USDatStorage storage $ = _getUSDatStorage();
+        if (!$.blacklisted[account]) revert("Not blacklisted");
+        $.blacklisted[account] = false;
         emit UnBlacklisted(account);
     }
 
     function isBlacklisted(address account) external view returns (bool) {
-        return _blacklisted[account];
+        USDatStorage storage $ = _getUSDatStorage();
+        return $.blacklisted[account];
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[49] private __gap;
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 }
